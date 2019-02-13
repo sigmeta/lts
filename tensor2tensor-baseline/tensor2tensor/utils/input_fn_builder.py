b584f475c6a46e348314064985fb03c661ed5aa1
Sigmeta <sigmeta@pku.edu.cn>
2019-02-13 15:53:34 +0800

    test bugs shape

--- tensor2tensor-baseline/tensor2tensor/utils/input_fn_builder.py
+++ tensor2tensor-baseline/tensor2tensor/utils/input_fn_builder.py
@@ -244,9 +244,9 @@ def features_for_problem(problem_instance,
       feature_map["inputs"] = tf.expand_dims(feature_map["inputs"], axis=-1)
   while len(feature_map["targets"].get_shape()) != 4:
     feature_map["targets"] = tf.expand_dims(feature_map["targets"], axis=-1)
-  while len(feature_map["teachers"].get_shape()) != 5:
+  while len(feature_map["teachers"].get_shape()) != 4:
     feature_map["teachers"] = tf.expand_dims(feature_map["teachers"], axis=-1)
-  feature_map["teachers"] = tf.reshape(feature_map["teachers"],[feature_map["teachers"].get_shape()[0],-1,1,1,76])
+  #feature_map["teachers"] = tf.reshape(feature_map["teachers"],[feature_map["teachers"].get_shape()[0],-1,1,1,76])
 
   if problem_instance.has_inputs:
     feature_map["input_space_id"] = tf.constant(p_hparams.input_space_id)
