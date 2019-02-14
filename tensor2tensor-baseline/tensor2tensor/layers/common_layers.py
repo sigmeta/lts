1e6ebcbaf41772e5ffcecd633d4fee94df346994
Sigmeta <sigmeta@pku.edu.cn>
2019-02-14 14:37:12 +0800

    reshape 74

--- tensor2tensor-baseline/tensor2tensor/layers/common_layers.py
+++ tensor2tensor-baseline/tensor2tensor/layers/common_layers.py
@@ -1445,7 +1445,6 @@ def weights_concatenated(labels):
 
 def padded_cross_entropy(logits,
                          labels,
-                         labels1,
                          label_smoothing,
                          weights_fn=weights_nonzero,
                          reduce_sum=True):
